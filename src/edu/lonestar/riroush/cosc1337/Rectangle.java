fa71ec6077ba25b2472ac55b320d30983a51092b
riroush <RIROUSH@d2l.lonestar.edu>
2016-03-27 19:50:40 -0700

    refactor to add rectangle

---
+++ src/edu/lonestar/riroush/cosc1337/Rectangle.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package edu.lonestar.riroush.cosc1337;
+
+/**
+ * @author rjroush
+ *
+ */
+public class Rectangle {
+	
+	private double width;
+	private double length;
+	
+	public Rectangle(double width, double length){
+		this.length =  length;
+		this.width = width;
+	
+	}
+
+	public double getWidth() {
+		return width;
+	}
+
+	public double getLength() {
+		return length;
+	}
+	public double getArea(){
+		return .5 * (width * length);
+	}
+	public String toString(){
+		return String.format("Rectangle width = %f\nRectangle length = %f\nRectangle area = %f", width, length, getArea()); 
+	}
+
+}
